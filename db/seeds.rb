ebed6d77a6bf108dffa3d08c101c8004d2c2f217
Alexander Baas <ajmbaas@outlook.com>
2017-10-13 18:47:00 +0200

    first seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Song.destroy_all
+Artist.destroy_all
+
+#artists
+artist1 = Artist.create!(name: "Bonobo", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+artist2 = Artist.create!(name: "Son Sinaa", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+artist3 = Artist.create!(name: "Onra", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+artist4 = Artist.create!(name: "TR/ST", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+artist5 = Artist.create!(name: "Clams Casino", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+artist6 = Artist.create!(name: "Emancipator", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+artist7 = Artist.create!(name: "Fatima Yamaha", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+artist8 = Artist.create!(name: "Gold Panda", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+artist9 = Artist.create!(name: "John Talabot", remote_image_url: "http://res.cloudinary.com/ajmbaas/image/upload/v1507900162/bonoboflashlightep_ozebrn.jpg")
+
+
+#songs
+Song.create!(
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+            {title: "trackname", artist:"Bonobo"},
+
+)
