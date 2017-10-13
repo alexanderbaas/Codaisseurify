bc7b2991d5f1205d9b3bd3bb8dc31c3d12e93442
Alexander Baas <ajmbaas@outlook.com>
2017-10-13 17:51:37 +0200

    create model Artist

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013154344) do
+ActiveRecord::Schema.define(version: 20171013155111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "remote_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "title"
---
+++ spec/factories/artists.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :artist do
+    name "MyString"
+    remote_image_url "MyString"
+  end
+end
